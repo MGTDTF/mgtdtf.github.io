7237ebec8f07bf54cc1a6930034d7998f04a9f2a
chris cullin <tf@miplace.com>
2024-10-12 21:48:51 +1100

    format fixes

--- scripts/tree.js
+++ scripts/tree.js
@@ -89,150 +89,150 @@ async function init() {
     
 //   }
     
-  function ParseTree(poNode, PsTree) {
-    var gNo;
-    var ItemCnt = 0;
-    var Len=poNode.childNodes.length; // �q�m�[�h���̎擾
-    var Buf="";
-    var CenterBuf = "";
-    var BeforeBuf="<div id="+ id;
-  
-    if(id>0) BeforeBuf+=" style='display:none'"; // �ŏ��̃m�[�h�ȊO�͑S�ĕ�����Ԃɂ���
-    BeforeBuf+=">";
-  
-    for(var i=0;i < Len;i++) {
-      nodename = poNode.childNodes[i].nodeName;
+function ParseTree(poNode, PsTree) {
+  var gNo;
+  var ItemCnt = 0;
+  var Len=poNode.childNodes.length; // �q�m�[�h���̎擾
+  var Buf="";
+  var CenterBuf = "";
+  var BeforeBuf="<div id="+ id;
+
+  if(id>0) BeforeBuf+=" style='display:none'"; // �ŏ��̃m�[�h�ȊO�͑S�ĕ�����Ԃɂ���
+  BeforeBuf+=">";
+
+  for(var i=0;i < Len;i++) {
+    nodename = poNode.childNodes[i].nodeName;
+    
+    if(nodename!="title" && nodename!="color" && nodename!="#text"){
+      // if not 'title' or 'color' then it's a menu, item or url
+      id++;
+      if(i==Len-1) gNo=0; else gNo=1; // �Ō�̃m�[�h�̏ꍇ gNo=0 ����ȊO gNo=1
+      gNoArray[id] = gNo;
       
-      if(nodename!="title" && nodename!="color" && nodename!="#text"){
-        // if not 'title' or 'color' then it's a menu, item or url
-        id++;
-        if(i==Len-1) gNo=0; else gNo=1; // �Ō�̃m�[�h�̏ꍇ gNo=0 ����ȊO gNo=1
-        gNoArray[id] = gNo;
-        
-        var sTree = PsTree + "<img src='"+dImg[gNo]+"' class='treeimg' align='top'>"; 
-        var child = poNode.childNodes.item(i); //menu item
-  
-        if(child.childNodes.length > 1){
-          if(child.childNodes[1].nodeName == "color"){
-            ColorFlag[id]=child.childNodes[1].textContent;
-          }else{
-            ColorFlag[id]=-1;
-          }
+      var sTree = PsTree + "<img src='"+dImg[gNo]+"' class='treeimg' align='top'>"; 
+      var child = poNode.childNodes.item(i); //menu item
+
+      if(child.childNodes.length > 1){
+        if(child.childNodes[1].nodeName == "color"){
+          ColorFlag[id]=child.childNodes[1].textContent;
         }else{
-            ColorFlag[id]=-1;
+          ColorFlag[id]=-1;
         }
-        
-        if(child.nodeName=="menu"){
-          var title = child.childNodes[1].textContent; //menu title
-          // �v�f����"menu"�̏ꍇ�̏���
-          openFlag[id]=true;
-            if (ColorFlag[id]==-1) {
-              Buf= PsTree + "<span class='fd' onclick='ViewTree("+id+","+gNo+")' title='" + title + "'><font class='treedata'><img id='Node"+id+"' src='"+pImg[gNo]+"' class='treeimg' align='top'>" + title + "</font></span><br>"; // \n
-            } else {
-              Buf= PsTree + "<span class='fd' onclick='ViewTree("+id+","+gNo+")' title='" + title + "'><font class='treedata'><img id='Node"+id+"' src='"+pImg[gNo]+"' class='treeimg' align='top'><img id='FD"+id+"' src='"+fImg[ColorFlag[id] * 2]+"' class='treeimg'  align='top'>" + title + "</font></span><br>"; 
-            }
-          if(child.hasChildNodes) {
-                var ChildBuf = ParseTree(child, sTree); //�q�m�[�h������ꍇ�A�ċA����
-                if (ChildBuf!=""){
-                  // �q�m�[�h�ɕ\���Ώۃf�[�^���������ꍇ
-                  CenterBuf+=Buf + ChildBuf;
-                }
-          }else{
-                CenterBuf=Buf;
-          }
-        }else if(child.nodeName=="item"){
-          linkid++;
-          var iNodeCnt = child.childNodes.length;
-          var sNodeName="";
-          var sNodeValue="";
-          var sDatatype="";
-          var sStDate="";
-          var sEdDate="";
-          for(var j=1;j < iNodeCnt;j+=2) {
-            sNodeName = child.childNodes[j].nodeName;
-            sNodeValue = child.childNodes[j].textContent;
-  
-            if(sNodeName=="url"){
-              var title = child.childNodes[j-1].textContent;
-               var url = HomePath + sNodeValue; // URL
-            }else if(sNodeName=="datatype"){
-              sDatatype = sNodeValue;      // �f�[�^�^�C�v�̎擾
-            }else if(sNodeName=="stdate"){
-              sStDate = sNodeValue;          // �K���J�n�N���̎擾
-            }else if(sNodeName=="eddate"){
-              sEdDate = sNodeValue;          // �K���I���N���̎擾
-            }
-          }
-          if(sDatatype=="sb"){
-            CenterBuf+= PsTree + "<font class='treedata'><img src='" + rImg[gNo] + "' class='treeimg' align='top'><a id='link" + linkid + "' onclick=\"linkclick('link" + linkid + "')\" title='" + title + "' class='" + sDatatype + "' href='"+url+"' target='pdf' class='pdf'>" + title + "</a></font><br>";
-          }else if(sDatatype=="sup"){
-            CenterBuf+= PsTree + "<font class='treedata'><img src='" + rImg[gNo] + "' class='treeimg' align='top'><a id='link" + linkid + "' onclick=\"linkclick('link" + linkid + "')\" title='" + title + "' class='" + sDatatype + "' href='"+url+"' target='pdf' class='pdf'>" + title + "</a></font><br>"; 
-          }else{
-            CenterBuf+= PsTree + "<font class='treedata'><img src='" + rImg[gNo] + "' class='treeimg' align='top'><a id='link" + linkid + "' onclick=\"linkclick('link" + linkid + "')\" title='" + title + "' href='" + url + "'  target='pdf' class='pdf'>" + title + "</a></font><br>";
-          }
-        }
-      } else {
-        // �v�f����"title" or "color" �̏ꍇ�͉����������Ȃ�
-        // do nothing
+      }else{
+          ColorFlag[id]=-1;
       }
-    }
-    if (CenterBuf!=""){
-      sRet = BeforeBuf + CenterBuf + "</div>";
-      return(sRet);
-    }else{
-      return("");
-    }
-  }
-  
-  var sBefore = "";
-  
-  function linkclick(psObj) {
-    if (sBefore != psObj && sBefore != "") document.all(sBefore).style.fontWeight = "normal";
-    document.all(psObj).style.fontWeight = "bold";
-    sBefore = psObj;
-  }
-  
-  function ViewTree(id,n) {
-    if(document.getElementById(id)){
-      if (window.getComputedStyle(document.getElementById(id)).display === "none"){
-        document.getElementById(id).style="display:block";
-        document.getElementById("Node"+id).src=mImg[n];
+      
+      if(child.nodeName=="menu"){
+        var title = child.childNodes[1].textContent; //menu title
+        // �v�f����"menu"�̏ꍇ�̏���
         openFlag[id]=true;
-      } else {
-        document.getElementById(id).style.display="none";
-        document.getElementById("Node"+id).src=pImg[n]
-        openFlag[id]=false;
+          if (ColorFlag[id]==-1) {
+            Buf= PsTree + "<span class='fd' onclick='ViewTree("+id+","+gNo+")' title='" + title + "'><font class='treedata'><img id='Node"+id+"' src='"+pImg[gNo]+"' class='treeimg' align='top'>" + title + "</font></span><br>"; // \n
+          } else {
+            Buf= PsTree + "<span class='fd' onclick='ViewTree("+id+","+gNo+")' title='" + title + "'><font class='treedata'><img id='Node"+id+"' src='"+pImg[gNo]+"' class='treeimg' align='top'><img id='FD"+id+"' src='"+fImg[ColorFlag[id] * 2]+"' class='treeimg'  align='top'>" + title + "</font></span><br>"; 
+          }
+        if(child.hasChildNodes) {
+              var ChildBuf = ParseTree(child, sTree); //�q�m�[�h������ꍇ�A�ċA����
+              if (ChildBuf!=""){
+                // �q�m�[�h�ɕ\���Ώۃf�[�^���������ꍇ
+                CenterBuf+=Buf + ChildBuf;
+              }
+        }else{
+              CenterBuf=Buf;
+        }
+      }else if(child.nodeName=="item"){
+        linkid++;
+        var iNodeCnt = child.childNodes.length;
+        var sNodeName="";
+        var sNodeValue="";
+        var sDatatype="";
+        var sStDate="";
+        var sEdDate="";
+        for(var j=1;j < iNodeCnt;j+=2) {
+          sNodeName = child.childNodes[j].nodeName;
+          sNodeValue = child.childNodes[j].textContent;
+
+          if(sNodeName=="url"){
+            var title = child.childNodes[j-1].textContent;
+              var url = HomePath + sNodeValue; // URL
+          }else if(sNodeName=="datatype"){
+            sDatatype = sNodeValue;      // �f�[�^�^�C�v�̎擾
+          }else if(sNodeName=="stdate"){
+            sStDate = sNodeValue;          // �K���J�n�N���̎擾
+          }else if(sNodeName=="eddate"){
+            sEdDate = sNodeValue;          // �K���I���N���̎擾
+          }
+        }
+        if(sDatatype=="sb"){
+          CenterBuf+= PsTree + "<font class='treedata'><img src='" + rImg[gNo] + "' class='treeimg' align='top'><a id='link" + linkid + "' onclick=\"linkclick('link" + linkid + "')\" title='" + title + "' class='" + sDatatype + "' href='"+url+"' target='pdf' class='pdf'>" + title + "</a></font><br>";
+        }else if(sDatatype=="sup"){
+          CenterBuf+= PsTree + "<font class='treedata'><img src='" + rImg[gNo] + "' class='treeimg' align='top'><a id='link" + linkid + "' onclick=\"linkclick('link" + linkid + "')\" title='" + title + "' class='" + sDatatype + "' href='"+url+"' target='pdf' class='pdf'>" + title + "</a></font><br>"; 
+        }else{
+          CenterBuf+= PsTree + "<font class='treedata'><img src='" + rImg[gNo] + "' class='treeimg' align='top'><a id='link" + linkid + "' onclick=\"linkclick('link" + linkid + "')\" title='" + title + "' href='" + url + "'  target='pdf' class='pdf'>" + title + "</a></font><br>";
+        }
       }
     } else {
-      console.log("menu id not found in ViewTree");
+      // �v�f����"title" or "color" �̏ꍇ�͉����������Ȃ�
+      // do nothing
     }
   }
+  if (CenterBuf!=""){
+    sRet = BeforeBuf + CenterBuf + "</div>";
+    return(sRet);
+  }else{
+    return("");
+  }
+}
   
-  function AllClose(){
-    var i = 0;
+var sBefore = "";
   
-    for (i = 0; i < openFlag.length; i++) {
-      if (openFlag[i] == true) {
-        document.getElementById("Node"+i).src=pImg[gNoArray[i]];
-        if (ColorFlag[i]!=-1) {
-          document.getElementById("FD"+i).src=fImg[ColorFlag[i] * 2];
-        }
-        document.getElementById(i).style="display:none";
-        openFlag[i]=false;
+function linkclick(psObj) {
+  if (sBefore != psObj && sBefore != "") document.all(sBefore).style.fontWeight = "normal";
+  document.all(psObj).style.fontWeight = "bold";
+  sBefore = psObj;
+}
+  
+function ViewTree(id,n) {
+  if(document.getElementById(id)){
+    if (window.getComputedStyle(document.getElementById(id)).display === "none"){
+      document.getElementById(id).style="display:block";
+      document.getElementById("Node"+id).src=mImg[n];
+      openFlag[id]=true;
+    } else {
+      document.getElementById(id).style.display="none";
+      document.getElementById("Node"+id).src=pImg[n]
+      openFlag[id]=false;
+    }
+  } else {
+    console.log("menu id not found in ViewTree");
+  }
+}
+  
+function AllClose(){
+  var i = 0;
+
+  for (i = 0; i < openFlag.length; i++) {
+    if (openFlag[i] == true) {
+      document.getElementById("Node"+i).src=pImg[gNoArray[i]];
+      if (ColorFlag[i]!=-1) {
+        document.getElementById("FD"+i).src=fImg[ColorFlag[i] * 2];
       }
+      document.getElementById(i).style="display:none";
+      openFlag[i]=false;
     }
   }
+}
     
-  function getParamValue(sGetString, sParamName) {
-    var sValue = "";
-    
-    if (sGetString.indexOf(sParamName) >= 0) {
-      sValue = sGetString.substring(sGetString.indexOf(sParamName) + sParamName.length);
-      if (sValue.indexOf("&") > 0) {
-        sValue = sValue.substring(0, sValue.indexOf("&"));
-      }
+function getParamValue(sGetString, sParamName) {
+  var sValue = "";
+  
+  if (sGetString.indexOf(sParamName) >= 0) {
+    sValue = sGetString.substring(sGetString.indexOf(sParamName) + sParamName.length);
+    if (sValue.indexOf("&") > 0) {
+      sValue = sValue.substring(0, sValue.indexOf("&"));
     }
-    return unescape(sValue);
   }
+  return unescape(sValue);
+}
   
   
